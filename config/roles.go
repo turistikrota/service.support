6f57c1cbc2ee7172ea30d531fc3bbe14e66e4e0c
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-09 23:47:34 +0300

    add base roles

--- config/roles.go
+++ config/roles.go
@@ -1,1 +1,25 @@
 package config
+
+import "github.com/turistikrota/service.shared/base_roles"
+
+type supportRoles struct {
+	ReadContact  string
+	ListContact  string
+	ReadFeedback string
+	ListFeedback string
+}
+
+type roles struct {
+	base_roles.Roles
+	Support supportRoles
+}
+
+var Roles = roles{
+	Roles: base_roles.BaseRoles,
+	Support: supportRoles{
+		ReadContact:  "support.contact.read",
+		ListContact:  "support.contact.list",
+		ReadFeedback: "support.feedback.read",
+		ListFeedback: "support.feedback.list",
+	},
+}
