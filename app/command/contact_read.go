753b1e1f956563d1d4d9f004f5a238eaf6589ff2
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-11-09 23:36:53 +0300

    add contact/feedback read cmd

--- app/command/contact_read.go
+++ app/command/contact_read.go
@@ -1,1 +1,23 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.support/domains/contact"
+)
+
+type ContactReadCmd struct {
+	UUID string `params:"uuid" validate:"required,object_id"`
+}
+
+type ContactReadRes struct{}
+
+type ContactReadHandler cqrs.HandlerFunc[ContactReadCmd, *ContactReadRes]
+
+func NewContactReadHandler(factory contact.Factory, repo contact.Repository) ContactReadHandler {
+	return func(ctx context.Context, cmd ContactReadCmd) (*ContactReadRes, *i18np.Error) {
+		return &ContactReadRes{}, repo.Read(ctx, cmd.UUID)
+	}
+}
--- app/command/feedback_read.go
+++ app/command/feedback_read.go
@@ -1,1 +1,23 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.support/domains/feedback"
+)
+
+type FeedbackReadCmd struct {
+	UUID string `params:"uuid" validate:"required,object_id"`
+}
+
+type FeedbackReadRes struct{}
+
+type FeedbackReadHandler cqrs.HandlerFunc[FeedbackReadCmd, *FeedbackReadRes]
+
+func NewFeedbackReadHandler(factory feedback.Factory, repo feedback.Repository) FeedbackReadHandler {
+	return func(ctx context.Context, cmd FeedbackReadCmd) (*FeedbackReadRes, *i18np.Error) {
+		return &FeedbackReadRes{}, repo.Read(ctx, cmd.UUID)
+	}
+}
