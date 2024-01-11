253dcd9693aaee58256b494a20e485ae0abb4edb
9ssi7 <info@ssibrahimbas.com>
2024-01-11 21:32:18 +0300

    feature/support: add close cmd

--- app/command/support_close.go
+++ app/command/support_close.go
@@ -1,1 +1,28 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.support/domains/support"
+)
+
+type SupportCloseCmd struct {
+	UUID     string `params:"uuid" validate:"required,object_id"`
+	UserUUID string `json:"-"`
+	UserName string `json:"-"`
+}
+
+type SupportCloseRes struct{}
+
+type SupportCloseHandler cqrs.HandlerFunc[SupportCloseCmd, *SupportCloseRes]
+
+func NewSupportCloseHandler(repo support.Repo) SupportCloseHandler {
+	return func(ctx context.Context, cmd SupportCloseCmd) (*SupportCloseRes, *i18np.Error) {
+		return &SupportCloseRes{}, repo.Close(ctx, cmd.UUID, support.WithUser{
+			UUID: cmd.UserUUID,
+			Name: cmd.UserName,
+		})
+	}
+}
