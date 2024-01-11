4f97d700123146c1404f32115d5ab421d96569d3
9ssi7 <info@ssibrahimbas.com>
2024-01-11 20:58:34 +0300

    feature/support: add admin delete command

--- app/command/support_admin_delete.go
+++ app/command/support_admin_delete.go
@@ -1,1 +1,23 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.support/domains/support"
+)
+
+type SupportAdminDeleteCmd struct {
+	UUID string `json:"-"`
+}
+
+type SupportAdminDeleteRes struct{}
+
+type SupportAdminDeleteHandler cqrs.HandlerFunc[SupportAdminDeleteCmd, *SupportAdminDeleteRes]
+
+func NewSupportAdminDeleteHandler(repo support.Repo) SupportAdminDeleteHandler {
+	return func(ctx context.Context, cmd SupportAdminDeleteCmd) (*SupportAdminDeleteRes, *i18np.Error) {
+		return &SupportAdminDeleteRes{}, repo.AdminDelete(ctx, cmd.UUID)
+	}
+}
