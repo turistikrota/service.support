da47da9a948e092c0f4ee23be5eb93f34e0bd64b
9ssi7 <info@ssibrahimbas.com>
2024-01-11 23:04:34 +0300

    feature/support: add detail query

--- domains/support/dtos.go
+++ domains/support/dtos.go
@@ -6,12 +6,18 @@ type ListDto struct{}
 
 type AdminDto struct{}
 
+type DetailDto struct{}
+
 func (e *Entity) ToList() ListDto {
 	return ListDto{}
 }
 
 func (e *Entity) ToAdmin() AdminDto {
 	return AdminDto{}
+}
+
+func (e *Entity) ToDetail() DetailDto {
+	return DetailDto{}
 }
 
 func (e *Entity) ToAdminList() AdminListDto {
