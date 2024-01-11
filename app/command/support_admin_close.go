a3cc509a5dae9dd85e635a7c4981a7a5636ba1d8
9ssi7 <info@ssibrahimbas.com>
2024-01-11 20:58:11 +0300

    feature/support: complete admin close cmd

--- app/command/support_admin_close.go
+++ app/command/support_admin_close.go
@@ -1,1 +1,23 @@
 package command
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.support/domains/support"
+)
+
+type SupportAdminCloseCmd struct {
+	UUID string `json:"-"`
+}
+
+type SupportAdminCloseRes struct{}
+
+type SupportAdminCloseHandler cqrs.HandlerFunc[SupportAdminCloseCmd, *SupportAdminCloseRes]
+
+func NewSupportAdminCloseHandler(repo support.Repo) SupportAdminCloseHandler {
+	return func(ctx context.Context, cmd SupportAdminCloseCmd) (*SupportAdminCloseRes, *i18np.Error) {
+		return &SupportAdminCloseRes{}, repo.AdminClose(ctx, cmd.UUID)
+	}
+}
