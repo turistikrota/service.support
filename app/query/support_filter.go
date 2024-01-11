de8caf1313be164d3d91127fce1aef6e49353217
9ssi7 <info@ssibrahimbas.com>
2024-01-11 22:54:14 +0300

    feature/support: add filter query

--- app/query/support_filter.go
+++ app/query/support_filter.go
@@ -1,1 +1,55 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/cilloparch/cillop/types/list"
+	"github.com/turistikrota/service.support/domains/support"
+	"github.com/turistikrota/service.support/pkg/utils"
+)
+
+type SupportFilterQuery struct {
+	*utils.Pagination
+	*support.FilterEntity
+	UserUUID string `json:"-"`
+	UserName string `json:"-"`
+}
+
+type SupportFilterRes struct {
+	List *list.Result[support.ListDto]
+}
+
+type SupportFilterHandler cqrs.HandlerFunc[SupportFilterQuery, *SupportFilterRes]
+
+func NewSupportFilterHandler(repo support.Repo) SupportFilterHandler {
+	return func(ctx context.Context, query SupportFilterQuery) (*SupportFilterRes, *i18np.Error) {
+		query.Default()
+		offset := (*query.Page - 1) * *query.Limit
+		res, err := repo.Filter(ctx, support.WithUser{
+			UUID: query.UserUUID,
+			Name: query.UserName,
+		}, *query.FilterEntity, list.Config{
+			Offset: offset,
+			Limit:  *query.Limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+		li := make([]support.ListDto, len(res.List))
+		for i, e := range res.List {
+			li[i] = e.ToList()
+		}
+		return &SupportFilterRes{
+			List: &list.Result[support.ListDto]{
+				List:          li,
+				Total:         res.Total,
+				FilteredTotal: res.FilteredTotal,
+				Page:          res.Page,
+				IsNext:        res.IsNext,
+				IsPrev:        res.IsPrev,
+			},
+		}, nil
+	}
+}
--- domains/support/dtos.go
+++ domains/support/dtos.go
@@ -2,6 +2,12 @@ package support
 
 type AdminListDto struct{}
 
+type ListDto struct{}
+
+func (e *Entity) ToList() ListDto {
+	return ListDto{}
+}
+
 func (e *Entity) ToAdminList() AdminListDto {
 	return AdminListDto{}
 }
